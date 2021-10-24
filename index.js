183c97fb7d68c43b835574cfd5b1d4e0a64d4f90
RomanVannieuwenhuyse <roman.vannieuwenhuyse@sint-pieterscollege.be>
2021-10-24 11:32:06 +0200

    Just testin'

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-var ip = require("ip");
-console.dir ( ip.address() );
-
 const PORT = process.env.PORT || 8080;
 
-var http = require("http");
-var server = http.createServer();
-server.listen(PORT);
+const hapi = require('hapi')
+
+const server = new hapi.Server()
+
+// Tell our app to listen on port 3000
+server.connection({ port: PORT })
+
+// Create the POST route to /sms
+server.route({
+  method: 'POST',
+  path: '/',
+  handler: function (request, reply) {
+    const body = request.payload.Body
+    const response = reply(`You sent: ${body} to Hapi`)
+    response.header('Content-Type', 'text/plain')
+  }
+})
+
+server.start(function (err) {
+  if (err) {
+    throw err
+  }
+})
