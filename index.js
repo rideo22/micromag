74f9b3ea8a781c4b9eff3e398a255f6c046b80d2
RomanVannieuwenhuyse <roman.vannieuwenhuyse@sint-pieterscollege.be>
2021-10-27 14:15:54 +0200

    Revert "Just testin'"
    This reverts commit 183c97fb7d68c43b835574cfd5b1d4e0a64d4f90.

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
+var ip = require("ip");
+console.dir ( ip.address() );
+
 const PORT = process.env.PORT || 8080;
 
-const hapi = require('hapi')
-
-const server = new hapi.Server()
-
-// Tell our app to listen on port 3000
-server.connection({ port: PORT })
-
-// Create the POST route to /sms
-server.route({
-  method: 'POST',
-  path: '/',
-  handler: function (request, reply) {
-    const body = request.payload.Body
-    const response = reply(`You sent: ${body} to Hapi`)
-    response.header('Content-Type', 'text/plain')
-  }
-})
-
-server.start(function (err) {
-  if (err) {
-    throw err
-  }
-})
+var http = require("http");
+var server = http.createServer();
+server.listen(PORT);
